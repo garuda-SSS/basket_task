b635cbc5733c0baefa2e261643c6b6a67f320a9e
garuda-SSS <sipin1997@gmail.com>
2023-09-04 22:16:55 +0300

    Тест переименован

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def test_abs1(browser):
+def test_button_exists(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(10)
